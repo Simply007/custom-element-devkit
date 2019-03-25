7578401497b90faeb1dfb637224b3bb224f1263c
Simply007 <chrastina.ondra@gmail.com>
2019-03-24 20:46:14 -0400

    adjust height + connect with appropriate styles

--- client/custom-elements/ToastUiEditor/components/toastUiEditor.tsx
+++ client/custom-elements/ToastUiEditor/components/toastUiEditor.tsx
@@ -9,7 +9,9 @@ import 'tui-editor/dist/tui-editor-extTable';
 import 'tui-editor/dist/tui-editor-extColorSyntax';
 import 'tui-editor/dist/tui-editor-extScrollSync';
 
+import 'tui-editor/dist/tui-editor-contents.min.css';
 import 'codemirror/lib/codemirror.css';
+import 'tui-chart/dist/tui-chart.min.css';
 import 'tui-editor/dist/tui-editor.min.css';
 import 'tui-color-picker/dist/tui-color-picker.min.css';
 
@@ -32,6 +34,8 @@ class ToastUiEditor extends React.Component {
           ref={this.editorRef}
           initialValue="Ahoy"
           onChange={this.storeValue}
+          height="400px"
+          // previewStyle="vertical"
           exts={[
             'chart',
             'scrollSync',
--- client/custom-elements/ToastUiEditor/editor.tsx
+++ client/custom-elements/ToastUiEditor/editor.tsx
@@ -10,7 +10,7 @@ import { tryParseJSON } from '../../../common/utils/utils';
 
 CustomElement.init((element, _context) => {
   // const value = tryParseJSON(element.value || '', {});
-  CustomElement.setHeight(((element.config || {}) as any).height || 600);
+  CustomElement.setHeight(((element.config || {}) as any).height || 400);
 
   // CustomElement.setValue(JSON.stringify(value));
 
